60a0ab169b0bcaea76a3e4b5271c536383442599
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-04-12 11:57:44 +0545

    Header completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,34 @@
+"use strict";
+
 /**
- * @license MIT
- * @author codewithsadee <mohammadsadee24@gmail.com>
- * @copyright codewithsadee 2023
- */
+ Add eventListener on multiple elements
+ *@param {nodelist} $elements Nodelist
+ *@param {string} eventType String
+ *@param {Function} callback function
+*/
 
+const addEventonElements = function ($elements, eventType, callback) {
+  for (const items of $elements) {
+    items.addEventListener(eventType, callback);
+  }
+};
 
-'use strict';
+/*
+Header Scroll state
+*/
+const $header = document.querySelector("[data-header]"); //NodeElement
+window.addEventListener("scroll", function () {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/* Search toggle */
+const $searchToggler = document.querySelector("[data-search-toggler]");
+const $searchField = document.querySelector("[data-search-field");
+let isExpanded = false; //boolean to check state if search field is expanded or not
+
+$searchToggler.addEventListener("click", function () {
+  $header.classList.toggle("search-active");
+  isExpanded = !isExpanded;
+  this.setAttribute("aria-expanded", isExpanded);
+  $searchField.focus();
+});
