73b4f42295f6a21bc24e5577690af67f510d2d45
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-04-12 22:02:25 +0545

    logic for Tab navigation added

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,3 +32,40 @@ $searchToggler.addEventListener("click", function () {
   this.setAttribute("aria-expanded", isExpanded);
   $searchField.focus();
 });
+
+/*
+Tab navigation
+*/
+const $tabBtns = document.querySelectorAll("[data-tab-btn]"); //nodeList
+const $tabPanels = document.querySelectorAll("[data-tab-panel]"); //nodeList
+
+let [$lastActiveTabBtn] = $tabBtns;
+let [$lastActiveTabPanel] = $tabPanels;
+
+addEventonElements($tabBtns, "click", function () {
+  $lastActiveTabBtn.setAttribute("aria-selected", "false");
+  $lastActiveTabPanel.setAttribute("hidden", "");
+  this.setAttribute("aria-selected", "true");
+  // get the panel name using aria-controls attribute to set the current panel
+  const $currentTabPanel = document.querySelector(
+    `#${this.getAttribute("aria-controls")}`
+  );
+  $currentTabPanel.removeAttribute("hidden");
+  $lastActiveTabBtn = this;
+  $lastActiveTabPanel = $currentTabPanel;
+});
+
+// keyboard accesibility for tab buttons
+addEventonElements($tabBtns, "keydown", function (e) {
+  const $nextElement = this.nextElementSibling;
+  const $previousElement = this.previousElementSibling;
+  if (e.key === "ArrowRight" && $nextElement) {
+    this.setAttribute("tabindex", "-1");
+    $nextElement.setAttribute("tabindex", "0");
+    $nextElement.focus();
+  } else if (e.key === "ArrowLeft" && $previousElement) {
+    this.setAttribute("tabindex", "-1");
+    $previousElement.setAttribute("tabindex", "0");
+    $previousElement.focus();
+  }
+});
