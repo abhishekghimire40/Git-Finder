a0cf4a8a3faf2aca0b65742da3c4b89756364e9d
Abhishek Ghimire <abhishek@pop-os.localdomain>
2023-04-13 16:20:37 +0545

    Search functionality and get profile data from api and loaded it in UI

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,23 @@
+"use strict";
+
 /**
- * @license MIT
- * @author codewithsadee <mohammadsadee24@gmail.com>
- * @copyright codewithsadee 2023
+ * Fetch data from server
+ * @param {*} url  API Url [required]
+ * @param {*} successCallback  success callback [required]
+ * @param {*} errorCallback  error callback [optional]
  */
 
-
-'use strict';
+export async function fetchData(url, successCallback, errorCallback) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      successCallback(data);
+    } else {
+      const error = await response.json();
+      errorCallback && errorCallback(error);
+    }
+  } catch (error) {
+    console.log("");
+  }
+}
